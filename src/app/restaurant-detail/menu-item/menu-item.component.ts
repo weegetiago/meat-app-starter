47cec860cb2a9d87474d3f7e353eb499ac63b3e3
Tiago.Weege <tiago.weege@senior.com.br>
2019-02-21 17:59:08 -0300

    #51

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core'
+
+import {MenuItem} from './menu-item.model'
 
 @Component({
   selector: 'mt-menu-item',
@@ -6,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuItemComponent implements OnInit {
 
+  @Input() menuItem: MenuItem //precisa do input pq é uma propriedade que o compronente parent vai informar pra vc
+  @Output() add = new EventEmitter()
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  emitAddEvent(){
+    this.add.emit(this.menuItem)
   }
 
 }
