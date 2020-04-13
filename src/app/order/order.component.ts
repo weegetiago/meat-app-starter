2c4ddd35d2ab0d2089b5c6449e94a6909ee1157c
weegetiago <weegetiago@gmail.com>
2020-04-13 10:09:04 -0300

    #74 - Reactive Forms

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,13 +4,19 @@ import { Component, OnInit } from '@angular/core';
 import { CartItem } from './../restaurant-detail/shopping-cart/cart-item.model';
 import { OrderService } from './order.service';
 import { PayOption } from './../shared/pay/pay-option.model';
-import {Order, OrderItem} from './order.model';
+import { Order, OrderItem } from './order.model';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html',
 })
 export class OrderComponent implements OnInit {
+
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  numberPattern = /^[0-9]*$/
+
+  orderform: FormGroup
 
   delivery: number = 8
 
@@ -22,10 +28,33 @@ export class OrderComponent implements OnInit {
 
   constructor(
     private orderService: OrderService,
-    private router: Router){}
+    private router: Router,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.orderform = this.formBuilder.group({
+      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
+      optionalAddress: this.formBuilder.control(''),
+      paymentOption: this.formBuilder.control('', [Validators.required]),
+    }, {validator: OrderComponent.equalsEmail})
   }
+
+  static equalsEmail(group: AbstractControl): {[key: string]: boolean} {
+    const email = group.get('email')
+    const emailConfirmation = group.get('emailConfirmation')
+    if (!email || !emailConfirmation) {
+      return undefined
+    }
+    if (email.value !== emailConfirmation.value) {
+      return { emailsNotMatch: true }
+    }
+    return undefined
+  }
+
 
   itemsValue(): number {
     return this.orderService.itemsValue()
@@ -43,17 +72,17 @@ export class OrderComponent implements OnInit {
     this.orderService.menosQtde(item)
   }
 
-  checkOrder(order: Order){
+  checkOrder(order: Order) {
     order.orderItems = this.cartItems()
-    .map((item:CartItem)=>new OrderItem(item.quantity, item.menuItem.id))
+      .map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
     this.orderService.checkOrder(order)
-    .subscribe((orderId: string) => {
-      this.router.navigate(['order-summary'])
-      console.log(`Compra conluída: ${orderId}`)
-      this.orderService.clear()
-    })
+      .subscribe((orderId: string) => {
+        this.router.navigate(['order-summary'])
+        //console.log(`Compra conluída: ${orderId}`)
+        this.orderService.clear()
+      })
 
-    console.log(order)
+    //console.log(order)
   }
 
   remove(item: CartItem) {
